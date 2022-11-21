9bbef96da4d7a36af11a69e1c310e1c54b96ea6e
Klay1001 <as0975790643@gmail.com>
2022-11-21 15:05:01 +0800

    1121 First Commit

---
+++ test1.py
@@ -0,0 +1,10 @@
+import math
+import random
+from scipy.stats import norm, zscore
+population=eval(input("有幾個同學呢: "))
+data = range(1,population+1)
+print("請回答問題: ",random.sample(data,1))
+
+print("test1test2test3")
+print("test1test2test3")
+print("test1test2test3")
